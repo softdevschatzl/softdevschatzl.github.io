028e33ed1b761efa249c9f32004310a3a938eb9a
theschatzinator <40133353+theschatzinator@users.noreply.github.com>
2023-07-09 22:28:25 -0400

    first commit of stuff, didnt even test it just added boilerplate

---
+++ newsoftdevschatz/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id = "app">
+    <Navbar />
+    <HomePage />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+import HomePage from './components/HomePage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    HomePage,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+}
+</style>
---
+++ newsoftdevschatz/src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav>
+        <ul>
+            <li><a href = "#">Home</a></li>
+            <li><a href = "#">About</a></li>
+            <li><a href = "#">Portfolio</a></li>
+            <li><a href = "#">Contact</a></li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped>
+nav {
+    background: #333;
+    padding: 1em;
+}
+
+nav ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: space-around;
+}
+
+nav a {
+    color: white;
+    text-decoration: none;
+}
+</style>
